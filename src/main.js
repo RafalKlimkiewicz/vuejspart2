d39de80f97f54bd3e6cd89d82077fb80d7be93b6
Rafal <klimek1008@gmail.com>
2024-09-21 17:27:31 +0200

    API DOM example

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css";
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
 
-Vue.config.productionTip = false
+let counter = 1;
+let container = document.createElement("div");
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+container.classList.add("text-center", "p-3");
+
+let msg = document.createElement("h1");
+
+msg.classList.add("bg-primary", "text-white", "p-3");
+msg.textContent = "Button has not beed clicked";
+
+let button = document.createElement("button");
+
+button.textContent = "Press me";
+button.classList.add("btn", "btn-secondary");
+
+button.onclick = () => msg.textContent = `Press count: ${counter++}`;
+
+container.appendChild(msg);
+container.appendChild(button);
+
+let app = document.getElementById("app");
+
+app.parentElement.replaceChild(container, app);
